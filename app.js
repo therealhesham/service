8f62eba886189d2c4205d6939455d04415b6cc4b
your.email@example.com <your.email@example.com>
2025-10-07 12:38:37 +0300

    It seems

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+// أي طلب يوصل هنا يتبعت للـ Service الداخلي
+app.use(express.json());
+
+app.all('/*', async (req, res) => {
+  try {
+    const serviceResponse = await axios({
+      method: req.method,
+      url: `http://localhost:4849`,
+      data: req.body
+    });
+    res.status(serviceResponse.status).send(serviceResponse.data);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Web App listening on port ${PORT}`);
+});
